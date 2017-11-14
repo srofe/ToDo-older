e8593b789a1d9092d75e0877e722bb29c6f48c16
Simon Rofe <simon.rofe@ieee.org>
2017-11-15 08:48:15 +1000

    Refactor to remove duplication
    The duplication in the test code is removed by creating test case
    variables, and using these it the tests. This also assists in avoiding
    typographical errors which may occur with individual strings.

--- ToDoTests/Models/ToDoItemTests.swift
+++ ToDoTests/Models/ToDoItemTests.swift
@@ -10,7 +10,11 @@ import XCTest
 @testable import ToDo
 
 class ToDoItemTests: XCTestCase {
-    
+
+    // Test variables used in the TestCase
+    let sutTitle = "Item Title"
+    let sutDescription = "A description of the ToDo item"
+
     override func setUp() {
         super.setUp()
     }
@@ -20,13 +24,13 @@ class ToDoItemTests: XCTestCase {
     }
 
     func testInitSetsTitle() {
-        let item = ToDoItem(title: "Item Title")
-        XCTAssertEqual("Item Title", item.title, "A ToDo shall set it's title when initialised.")
+        let item = ToDoItem(title: sutTitle)
+        XCTAssertEqual(sutTitle, item.title, "A ToDo shall set it's title when initialised.")
     }
 
     func testInitSetsDescriptionIfProvidedInInitialiser() {
-        let item = ToDoItem(title: "Item Title", description: "A description of the ToDo item")
-        XCTAssertEqual("A description of the ToDo item", item.description, "A ToDo shall set it's description if it is provided in the initialiser.")
+        let item = ToDoItem(title: sutTitle, description: sutDescription)
+        XCTAssertEqual(sutDescription, item.description, "A ToDo shall set it's description if it is provided in the initialiser.")
     }
 
 }
