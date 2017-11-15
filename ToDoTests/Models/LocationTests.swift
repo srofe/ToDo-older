dfe2491f69a776fbf83aa2a2b89b742056f977a6
Simon Rofe <simon.rofe@ieee.org>
2017-11-15 11:25:23 +1000

    Add test harness for Location
    Test for coordinate, then implement coordinate for Location.

---
+++ ToDoTests/Models/LocationTests.swift
@@ -0,0 +1,31 @@
+//
+//  LocationTests.swift
+//  ToDoTests
+//
+//  Created by Simon Rofe on 15/11/17.
+//  Copyright © 2017 Simon Rofe. All rights reserved.
+//
+
+import XCTest
+import CoreLocation
+@testable import ToDo
+
+class LocationTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testInitSetsCoordinate() {
+        let coordinate = CLLocationCoordinate2D(latitude: 1, longitude: 2)
+        let location = Location(name: "Location Name", coordinate: coordinate)
+        XCTAssertEqual(coordinate.latitude, location.coordinate?.latitude, "A Location shall set the coordinate's latitue when initialised.")
+        XCTAssertEqual(coordinate.longitude, location.coordinate?.longitude, "A Location shall set the coordinate's longitude when initialised.")
+
+    }
+
+}
