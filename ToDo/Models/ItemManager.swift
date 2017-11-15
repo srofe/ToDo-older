2fdcc8b10de6337dffb4881bc42fbee0f52c21b8
Simon Rofe <simon.rofe@ieee.org>
2017-11-16 07:55:02 +1000

    Refactor to remove duplication
    The duplication between test and application code was in the form of:
      - ToDoItem(title: "Item Name")
    Refactoring should remove duplication - either in the application code,
    the test code or between application and test code.

--- ToDo/Models/ItemManager.swift
+++ ToDo/Models/ItemManager.swift
@@ -11,12 +11,14 @@ import Foundation
 class ItemManager {
     var toDoCount = 0
     let doneCount = 0
+    private var toDoItems: [ToDoItem] = []
 
     func add(item newItem: ToDoItem) {
         toDoCount += 1
+        toDoItems.append(newItem)
     }
 
     func item(at index: Int) -> ToDoItem {
-        return ToDoItem(title: "Item Name")
+        return toDoItems[index]
     }
 }
