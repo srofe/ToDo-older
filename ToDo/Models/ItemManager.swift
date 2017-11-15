c9422e68187e2487539fead8ac6ef30059cc4aae
Simon Rofe <simon.rofe@ieee.org>
2017-11-16 07:13:59 +1000

    Introduce ItemManager class
    The ItemManager shall manage the collection of ToDo items. The
    ItemManager needs to provide a count of the items to be done.

---
+++ ToDo/Models/ItemManager.swift
@@ -0,0 +1,13 @@
+//
+//  ItemManager.swift
+//  ToDo
+//
+//  Created by Simon Rofe on 16/11/17.
+//  Copyright © 2017 Simon Rofe. All rights reserved.
+//
+
+import Foundation
+
+class ItemManager {
+    let toDoCount = 0
+}
---
+++ ToDoTests/Models/ItemManagerTests.swift
@@ -0,0 +1,28 @@
+//
+//  ItemManagerTests.swift
+//  ToDoTests
+//
+//  Created by Simon Rofe on 15/11/17.
+//  Copyright © 2017 Simon Rofe. All rights reserved.
+//
+
+import XCTest
+@testable import ToDo
+
+class ItemManagerTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testTodoCountInitiallySetToZero() {
+        let sut = ItemManager()
+        XCTAssertEqual(0, sut.toDoCount, "An ItemManger shall initialise the count of ToDo items to zero.")
+    }
+}
