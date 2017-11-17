887773363fa465b8edbd07fcfbf714a4d9be157e
Simon Rofe <simon.rofe@ieee.org>
2017-11-17 10:48:59 +1000

    Create test harness for ItemListDataProvider

---
+++ ToDoTests/Controllers/ItemListDataProviderTests.swift
@@ -0,0 +1,21 @@
+//
+//  ItemListDataProviderTests.swift
+//  ToDoTests
+//
+//  Created by Simon Rofe on 17/11/17.
+//  Copyright © 2017 Simon Rofe. All rights reserved.
+//
+
+import XCTest
+@testable import ToDo
+
+class ItemListDataProviderTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+}
