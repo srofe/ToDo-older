5dbb1147091349b23af4c7bac62d47b0011d65ef
Simon Rofe <simon.rofe@ieee.org>
2017-11-21 14:38:17 +1000

    Add label to ItemCell

--- ToDo/Views/ItemCell.swift
+++ ToDo/Views/ItemCell.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ItemCell: UITableViewCell {
 
+    @IBOutlet weak var titleLabel: UILabel!
+
     func configCell(with: ToDoItem) {
     }
 
---
+++ ToDoTests/Views/ItemCellTests.swift
@@ -0,0 +1,47 @@
+//
+//  ItemCellTests.swift
+//  ToDoTests
+//
+//  Created by Simon Rofe on 21/11/17.
+//  Copyright © 2017 Simon Rofe. All rights reserved.
+//
+
+import XCTest
+@testable import ToDo
+
+class ItemCellTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testHasNameLabel() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "ItemListViewController") as! ItemListViewController
+        controller.loadViewIfNeeded()
+        let tableView = controller.tableView
+        let dataSource = FakeDataSource()
+        tableView?.dataSource = dataSource
+        let cell = tableView?.dequeueReusableCell(withIdentifier: "ItemCell", for: IndexPath(row: 0, section: 0)) as! ItemCell
+        XCTAssertNotNil(cell.titleLabel, "An ItemCell shall have a title label.")
+    }
+}
+
+extension ItemCellTests {
+
+    class FakeDataSource: NSObject, UITableViewDataSource {
+
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 1
+        }
+
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            return UITableViewCell()
+        }
+    }
+
+}
