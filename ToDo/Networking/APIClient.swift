a3099131c0fb3374a9fb748a28bd9dcd665f6ccb
Simon Rofe <simon.rofe@ieee.org>
2018-01-06 16:32:01 +1000

    Add initial tests and API for URL handling
    Note: A mock URLSession is used as a test double so a real URL is not
    required for testing purposes.

---
+++ ToDo/Networking/APIClient.swift
@@ -0,0 +1,26 @@
+//
+//  APIClient.swift
+//  ToDo
+//
+//  Created by Simon Rofe on 6/1/18.
+//  Copyright © 2018 Simon Rofe. All rights reserved.
+//
+
+import Foundation
+
+class APIClient {
+    lazy var session: SessionProtocol = URLSession.shared
+
+    func loginUser(withName username: String, password: String, completion: @escaping (Token?, Error?) -> Void) {
+        guard let url = URL(string: "https://awsometodos.com") else { fatalError() }
+        session.dataTask(with: url) { (data, response, error) in
+            
+        }
+    }
+}
+
+protocol SessionProtocol {
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
+}
+
+extension URLSession: SessionProtocol {}
---
+++ ToDoTests/Networking/APIClientTests.swift
@@ -0,0 +1,46 @@
+//
+//  APIClientTests.swift
+//  ToDoTests
+//
+//  Created by Simon Rofe on 6/1/18.
+//  Copyright © 2018 Simon Rofe. All rights reserved.
+//
+
+import XCTest
+@testable import ToDo
+
+class APIClientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testLoginUsesExpectedHost() {
+        let sut = APIClient()
+        let mockURLSession = MockURLSession()
+        sut.session = mockURLSession
+        let completion = { (token: Token?, error: Error?) in }
+        sut.loginUser(withName: "dasdom", password: "1234", completion: completion)
+        guard let url = mockURLSession.url else { XCTFail(""); return }
+        let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true)
+    
+        XCTAssertEqual(urlComponents?.host, "awsometodos.com")
+    }
+}
+
+class MockURLSession: SessionProtocol {
+
+    var url: URL?
+
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        self.url = url
+
+        return URLSession.shared.dataTask(with: url)
+    }
+}
